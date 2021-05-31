9462513ec93d515a53ec3e525e9cc8d594878ea8
caoweijun <caoweijun@xiao100.com>
2021-05-31 18:13:37 +0800

    debug

--- packages/react-dom/src/client/ReactDOMLegacy.js
+++ packages/react-dom/src/client/ReactDOMLegacy.js
@@ -190,6 +190,7 @@ function legacyRenderSubtreeIntoContainer(
   let fiberRoot;
   if (!root) {
     // Initial mount
+    debugger
     root = container._reactRootContainer = legacyCreateRootFromDOMContainer(
       container,
       forceHydrate,
