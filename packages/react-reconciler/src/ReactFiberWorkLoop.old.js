fc24dcee2ba48883538f15d63b62c62e6371bc87
Synw <synw1024@yeah.net>
2022-01-21 15:14:36 +0800

    add debugger

--- packages/react-reconciler/src/ReactFiberWorkLoop.old.js
+++ packages/react-reconciler/src/ReactFiberWorkLoop.old.js
@@ -996,6 +996,7 @@ function markRootSuspended(root, suspendedLanes) {
 // This is the entry point for synchronous tasks that don't go
 // through Scheduler
 function performSyncWorkOnRoot(root) {
+  debugger
   if (enableProfilerTimer && enableProfilerNestedUpdatePhase) {
     syncNestedUpdateFlag();
   }
